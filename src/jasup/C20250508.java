f5d0d1e8185b6690e93223ec4ff20c8a6ed79ffb
kikyungchan <kikyungchan@gmail.com>
2025-05-08 17:48:27 +0900

    5월8일

--- src/jasup/C20250508.java
+++ src/jasup/C20250508.java
@@ -1,27 +1,63 @@
 package jasup;
 
+import ch06.sec04.Student;
+
 public class C20250508 {
     public static void main(String[] args) {
+        student s1 = new student("홍길동", 16, 2);
+        student s2 = new student("이영희", 15, 1);
 
+        s1.displayInfo();
+        s2.displayInfo();
+
+        s1.updateAge(17);
+        s2.updateAge(18);
+
+        System.out.println("\n업데이트된 학생 정보:");
+        s1.displayInfo();
+        s2.displayInfo();
     }
 }
 
-class BookClass01 {
-    String title;
-    String author;
-    int price;
+class student {
+    String name;
+    int age;
+    int grade;
 
-    public BookClass01(String title) {
-        this(title, "작자미상", 0);
+    public student(String name, int age, int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
     }
 
-    public BookClass01(String title, String author) {
-        this(title, author, 0);
+    public void updateAge(int newage) {
+        this.age = newage;
     }
 
-    public BookClass01(String title, String author, int price) {
-        this.title = title;
-        this.author = author;
-        this.price = price;
+    void displayInfo() {
+        System.out.println("이름 = " + name);
+        System.out.println("나이 = " + age);
+        System.out.println("학년 = " + grade);
     }
 }
+
+//class BookClass01 {
+//    String title;
+//    String author;
+//    int price;
+//
+//    public BookClass01(String title) {
+//        this(title, "작자미상", 0);
+//    }
+//
+//    public BookClass01(String title, String author) {
+//        this(title, author, 0);
+//    }
+//
+//    public BookClass01(String title, String author, int price) {
+//        this.title = title;
+//        this.author = author;
+//        this.price = price;
+//    }
+//
+//}
