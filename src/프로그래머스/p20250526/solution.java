5d27f138ac7adbabe4db8407968c5999acbf1ff0
kikyungchan <kikyungchan@gmail.com>
2025-05-26 17:49:29 +0900

    5월26일

---
+++ src/프로그래머스/p20250526/solution.java
@@ -0,0 +1,9 @@
+package 프로그래머스.p20250526;
+
+public class solution {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+//        System.out.println("3" + "\n", "2" + "\n", "1");
+        System.out.println(message);
+    }
+}
