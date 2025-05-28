98ed7489dce57840d11172804943c6dcdaf9e1d7
kikyungchan <kikyungchan@gmail.com>
2025-05-28 17:48:33 +0900

    5월28일

--- src/자습/프로그래머스/코딩기초문제/모음제거/Solution.java
+++ src/자습/프로그래머스/코딩기초문제/모음제거/Solution.java
@@ -2,11 +2,17 @@ package 자습.프로그래머스.코딩기초문제.모음제거;
 
 class Solution {
     public String solution(String my_string) {
-        //영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다.
-        // 모음을 제거한 문자열을 return.
         String answer = "";
+        //전체탐색하면서
+        //문자열 = 문자로
+        //조건확인
+        //해당값리턴
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if (!(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')) {
+                answer += c;
+            }
+        }
         return answer;
-        //문자열 탐색해서 char로
-        // 모음이 포함(contains)?되있으면
     }
 }
