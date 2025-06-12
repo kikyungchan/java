7e4b5a49fd83a97ea3c3a25259cef7b8f4520d47
kikyungchan <kikyungchan@gmail.com>
2025-06-12 17:48:18 +0900

    6월12일

--- src/자습/프로그래머스/코딩기초문제/문자열정렬하기2/Solution.java
+++ src/자습/프로그래머스/코딩기초문제/문자열정렬하기2/Solution.java
@@ -4,11 +4,14 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        String lowerCase = my_string.toLowerCase();
-        char[] chars = my_string.toCharArray();
-        Arrays.sort(chars);
 
-        return new String(chars);
+        String lowerCase = my_string.toLowerCase();
+
+        char[] a = lowerCase.toCharArray();
+        Arrays.sort(a);
+        String answer = new String(a);
+
+
+        return answer;
     }
 }
